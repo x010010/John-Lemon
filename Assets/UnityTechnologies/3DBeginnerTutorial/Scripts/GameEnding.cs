1085f27640f82dd39fc87b6fa3cc71807dbc688b
RaeJ25 <149260449+RaeJ25@users.noreply.github.com>
2023-12-05 17:28:37 -0500

    Add files via upload

---
+++ Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/GameEnding.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1.0f;
+    public float displayImageDuration = 1.0f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+
+    float m_Timer;
+  
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (m_IsPlayerAtExit)
+        {
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        }
+      else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
+        }
+
+    }
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
+    {
+        m_Timer += Time.deltaTime;
+       imageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+         
+        }
+    }
+}
